e2926463177154fcf50edc4cf532d5b0bde2403f
sakurrr <sakratliey11@gmail.com>
2024-03-07 16:50:05 +0900

    ログイン画面完成

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,16 +1,33 @@
-import { Button } from "@mui/material";
+"use client"
+import { Button, TextField } from "@mui/material";
+import { useState } from "react";
 
-export default function login () {
+export default function login() {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleEmailChange(e) {
+        setEmail(e.target.value);
+    }
+    function handlePasswordChange(e) {
+        setPassword(e.target.vaiue);
+    }
+
     return (
         <main>
-            <div style={{textAlign: "center", width: "50",
-        height: "30px"}}>
-                <input title="ログインID" atti="" class=""></input>
+            <p style={{textAlign: "center",fontSize: "60px"}}>
+                ログイン
+            </p>
+            <div style={{
+                textAlign: "center", alignItems:"center",width: "50", justifyContent:"center" ,height: "30px"
+            }}>
+                <TextField  value={email} onChange={handleEmailChange} label="メールアドレス" sx={{width: "500px", marginBottom:'20px'}}/>
                 <br></br>
-                <input title="パスワード" atti="" class=""></input>
+                <TextField  value={password} onChange={handlePasswordChange}  label="パスワード" sx={{width: "500px" ,marginBottom:"20px"}}/>
                 <br></br>
-            
-            <Button variant="contained" color="warning" >ログイン</Button>
+                <Button variant="outlined" color="warning" sx={{marginRight:"100px"}}>新規作成</Button>
+                <Button variant="contained" color="warning" >ログイン</Button>
             </div>
         </main>
     )
