1601b439c6e2f6697362fadbe88e34fb0ef41186
unknown <tsurutsuru275@gmail.com>
2024-03-07 15:14:21 +0900

    signup作成

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,11 +1,40 @@
+"use client"
+
+import { Button, TextField } from "@mui/material";
+import { useState } from "react";
+
 export default function signup () {
+
+    const [name,setName] = useState('')
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+
+    const handleNameChange = (e) => {
+        setName(e.target.value)
+    }
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
+
+
     return (
         <main>
-            <div>
-                <p>
-                    signup
-                </p>
+            <div style={{width:'full'  ,display:"flex" ,justifyContent:"center" ,alignItems:"center" }}>
+                <div style={{width:"30%",display:"flex" ,flexDirection:"column" ,gap:"20px" , alignItems:"center"}}>
+                    <p style={{fontSize:"60px"}}>
+                        SignUp
+                    </p>
+                    <TextField fullWidth value={name} onChange={handleNameChange} label="名前"/>
+                    <TextField fullWidth value={email} onChange={handleEmailChange} label="メールアドレス"/>
+                    <TextField fullWidth value={password} onChange={handlePasswordChange} label="パスワード"/>
+                    <div style={{display : "flex" ,  width:"100%",justifyContent : "space-between" , gap:"60px"}}>
+                        <Button variant="outlined" color="warning" fullWidth>戻る</Button>
+                        <Button variant="contained" color="warning" fullWidth>登録</Button>
+                    </div>
+                </div>
             </div>
         </main>
-    )
-}
+    )}
--- styles/globals.js
+++ styles/globals.js
@@ -1,3 +1,3 @@
-export const backgroundColor = '#F8F2E2'
+export const backgroundColor = '#000000'
 export const primaryColor = 'F8CE3As'
 
