4fae578b796fac4f698968347b13b59bef4059ff
unknown <tsurutsuru275@gmail.com>
2024-03-08 09:19:13 +0900

    username

--- components/SignupPage.jsx
+++ components/SignupPage.jsx
@@ -2,15 +2,15 @@
 
 import { Button, TextField } from "@mui/material";
 import { useState } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
 
-export default function SignupPage () {
+export default function SignupPage() {
 
-  
 
-    const [name,setName] = useState('')
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
     const handleNameChange = (e) => {
         setName(e.target.value)
@@ -24,37 +24,41 @@ export default function SignupPage () {
 
     const doRegister = () => {
         const auth = getAuth();
-    
+
         // Firebaseで用意されているユーザー登録の関数
         createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          // ユーザー登録すると自動的にログインされてuserCredential.userでユーザーの情報を取得できる
-          const user = userCredential.user;
-          // ユーザー登録ができたかどうかをわかりやすくするためのアラート
-          alert( '登録完了！' );
-          console.log( user );
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-      }
+            .then((userCredential) => {
+                // ユーザー登録すると自動的にログインされてuserCredential.userでユーザーの情報を取得できる
+                const user = userCredential.user;
+                updateProfile(user, {
+                    displayName: name
+                })
+                // ユーザー登録ができたかどうかをわかりやすくするためのアラート
+                alert('登録完了！');
+                console.log(user);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
 
     return (
         <main>
-            <div style={{width:'full'  ,display:"flex" ,justifyContent:"center" ,alignItems:"center" }}>
-                <div style={{width:"30%",display:"flex" ,flexDirection:"column" ,gap:"20px" , alignItems:"center"}}>
-                    <p style={{fontSize:"60px"}}>
+            <div style={{ width: 'full', display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <div style={{ width: "30%", display: "flex", flexDirection: "column", gap: "20px", alignItems: "center" }}>
+                    <p style={{ fontSize: "60px" }}>
                         SignUp
                     </p>
-                    <TextField fullWidth value={name} onChange={handleNameChange} label="名前"/>
-                    <TextField fullWidth value={email} onChange={handleEmailChange} label="メールアドレス"/>
-                    <TextField fullWidth value={password} onChange={handlePasswordChange} label="パスワード"/>
-                    <div style={{display : "flex" ,  width:"100%",justifyContent : "space-between" , gap:"60px"}}>
+                    <TextField fullWidth value={name} onChange={handleNameChange} label="名前" />
+                    <TextField fullWidth value={email} onChange={handleEmailChange} label="メールアドレス" />
+                    <TextField fullWidth value={password} onChange={handlePasswordChange} label="パスワード" />
+                    <div style={{ display: "flex", width: "100%", justifyContent: "space-between", gap: "60px" }}>
                         <Button variant="outlined" color="warning" fullWidth >戻る</Button>
                         <Button variant="contained" color="warning" fullWidth onClick={doRegister}>登録</Button>
                     </div>
                 </div>
             </div>
         </main>
-    )}
+    )
+}
